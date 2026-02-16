f30b0c4cd6dc34311236b2557fbf4990ab9802c6
jaywantpatel <jaywantpatel467@gmail.com>
2026-02-16 18:15:05 -0500

    Added Setmasks and Clear masks along with macros for them, also made a file called hashkeys that will be used to generate posKey to represent board state

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc cengine.c init.c bitboards.c -o cengine
+	gcc cengine.c init.c bitboards.c hashkeys.c -o cengine
--- cengine.c
+++ cengine.c
@@ -1,28 +1,19 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "defs.h"
 
 int main() {
 
     AllInit();
-
+    int index = 0;
     U64 playBitBoard = 0ULL;
 
-    playBitBoard |= (1ULL << SQ64(D2));
-    playBitBoard |= (1ULL << SQ64(D3));
-    playBitBoard |= (1ULL << SQ64(D4));
-
-    printf("\n");
-    PrintBitBoard(playBitBoard);
+    for ( index = 0; index < 64; index++){
+        printf("Index: %d\n", index);
+        PrintBitBoard(SetMask[index]);
+        printf("\n");
+    }
     
-    int count = CNT(playBitBoard);
-    printf("Count: %d\n", count);
-
-    printf("\n");
-    int index = POP(&playBitBoard);
-    printf("INDEX: %d\n", index);
-    PrintBitBoard(playBitBoard);
-    count = CNT(playBitBoard);
-    printf("Count: %d\n", count);
 
     return 0;
 }
